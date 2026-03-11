7edc5219a936048e0f67fa29ff73b60eab401e49
lhelina <hilinaashenafi38@gmail.com>
2026-03-11 22:56:34 +0300

    Time: 262 ms (48.91%), Space: 13.5 MB (63.04%) - LeetHub

---
+++ 0661-image-smoother/0661-image-smoother.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def imageSmoother(self, img):
+        """
+        :type img: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        rows=len(img)
+        cols=len(img[0])
+        result= [[0]*cols for _ in range(rows)]
+        for r in range(rows):
+            for c in range(cols):
+                total_sum=0
+                count=0
+                for i in range(r-1,r+2):
+                    for j in range(c-1,c+2):
+                        if 0<=i<rows and 0<=j<cols:
+                            total_sum+=img[i][j]
+                            count+=1
+
+                       
+                result[r][c]=total_sum//count
+        return result                
+
+
+        
