180e4a4d885f84cff55e5bb1ef87a5385f86f80d
lhelina <hilinaashenafi38@gmail.com>
2026-03-26 23:38:54 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -4,17 +4,16 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
-        rows = set()
-        cols = set()
+        index_rows = set()
+        index_cols = set()
         for r in range(len(matrix)):
             for c in range(len(matrix[0])):
-                if matrix[r][c] == 0:
-                    rows.add(r)
-                    cols.add(c)
-
-        for r in rows:
-            matrix[r] = [0] * len(matrix[0])
-
-        for c in cols:
+                if matrix[r][c]==0:
+                    index_rows.add(r)
+                    index_cols.add(c)
+        for r in index_rows:
+            matrix[r]=[0]*len(matrix[0])    
+        for c in index_cols:
             for r in range(len(matrix)):
-                matrix[r][c] = 0
+                matrix[r][c]=0
+        
