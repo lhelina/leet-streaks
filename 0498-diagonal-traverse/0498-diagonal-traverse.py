47201c1e225c9450761b68f831b4ac0ee74199e5
lhelina <hilinaashenafi38@gmail.com>
2026-03-27 23:26:36 +0300

    Time: 19 ms (47.67%), Space: 16 MB (13%) - LeetHub

---
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def findDiagonalOrder(self, mat):
+        """
+        :type mat: List[List[int]]
+        :rtype: List[int]
+        """
+        diagonals = defaultdict(list)
+        
+        for r in range(len(mat)):
+            for c in range(len(mat[0])):
+                diagonals[r + c].append(mat[r][c])
+        
+        result = []
+        
+        for k in range(len(mat) + len(mat[0]) - 1):
+            if k % 2 == 0:
+                result.extend(diagonals[k][::-1])  # reverse
+            else:
+                result.extend(diagonals[k])
+        
+        return result
