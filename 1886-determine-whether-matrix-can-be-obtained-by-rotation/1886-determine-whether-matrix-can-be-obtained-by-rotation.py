2f64b10d413baf508f2b0c85f81c13c8e2b5888a
lhelina <hilinaashenafi38@gmail.com>
2026-04-01 23:42:37 +0300

    Time: 0 ms (100%), Space: 12.3 MB (53.95%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.py
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.py
@@ -6,11 +6,10 @@ class Solution(object):
         :rtype: bool
         """
         def rotate(matrix):
-            n = len(matrix)
-            for i in range(n):
-                for j in range(i, n):
+           
+            for i in range(len(matrix)):
+                for j in range(i,len(matrix)):
                     matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-            
             for row in matrix:
                 row.reverse()
         
@@ -19,4 +18,4 @@ class Solution(object):
                 return True
             rotate(mat)
         
-        return False
+        return False       
