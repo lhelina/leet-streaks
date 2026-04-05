92a71b9964a415d70d41b7dfe4261befb72df71d
lhelina <hilinaashenafi38@gmail.com>
2026-04-05 23:57:10 +0300

    Time: 3 ms (72.17%), Space: 12.3 MB (68.7%) - LeetHub

--- 0969-pancake-sorting/0969-pancake-sorting.py
+++ 0969-pancake-sorting/0969-pancake-sorting.py
@@ -4,22 +4,16 @@ class Solution(object):
         :type arr: List[int]
         :rtype: List[int]
         """ 
-        result = []
-        n = len(arr)
-
-        for size in range(n, 1, -1):
-
-            max_num = max(arr[:size])
-            max_index = arr.index(max_num)
-
+        result=[]
+        n=len(arr)
+        for size in range(n,1,-1):
+            max_num=max(arr[:size])
+            max_index=arr.index(max_num)
             if max_index == size - 1:
                 continue
-
-            if max_index != 0:
-                arr[:max_index+1] = arr[:max_index+1][::-1]
-                result.append(max_index + 1)
-
+            if max_index!=0:
+                arr[:max_index+1]=arr[:max_index+1][::-1]
+                result.append(max_index+1)
             arr[:size] = arr[:size][::-1]
             result.append(size)
-
-        return result
+        return result    
